b816254636a9e4aec6936dcd7af22f68f830d4a9
Brave Chai <bravetsia@gmail.com>
2016-12-22 17:08:27 +0800

    build the initial framework of the koa project

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-app.js
+'use strict'
+
+var koa = require('koa')
+var logger = require('koa-logger')
+var session = require('koa-session')
+var bodyParser = require('koa-bodyParser')
+var app = koa()
+
+app.keys = ['imooc']
+app.use(logger())
+app.use(session(app))
+app.use(bodyParser())
+
+var router = require('./config/routers')()
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(1234)
+console.log('Listening: 1234')
