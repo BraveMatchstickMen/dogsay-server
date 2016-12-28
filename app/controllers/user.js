f53182e427d1ffc55cb903f19029b116956446af
Brave Chai <bravetsia@gmail.com>
2016-12-28 18:03:16 +0800

    configure mongodb model in koa

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,6 +1,36 @@
 "use strict"
 
+var xss = require('xss')
+var mongoose = require('mongoose')
+var User = mongoose.model('User')
+
 exports.signup = function *(next) {
+	// var phoneNumber = this.request.body.phoneNumber
+	var phoneNumber = this.query.phoneNumber
+
+	var user = yield User.findOne({
+		phoneNumber: phoneNumber
+	}).exec()
+
+	if (!user) {
+		user = new User({
+			phoneNumber: xss(phoneNumber)
+		})
+	}
+	else {
+		user.verifyCode = '1221'
+	}
+
+	try {
+	  user = yield user.save()
+	}
+	catch(e) {
+	  this.body = {
+		success: false
+	  }
+	  return
+	}
+
 	this.body = {
 		success: true
 	}
