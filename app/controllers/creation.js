c84170e811b05b5d6d099157585380da847c04f5
Brave Chai <brave.chai@blackboard.com>
2017-10-26 15:37:20 +0800

    synchronize customary video to server

--- app/controllers/creation.js
+++ app/controllers/creation.js
@@ -3,9 +3,11 @@
 var mongoose = require('mongoose')
 
 var User = mongoose.model('User')
-var Promise = require('bluebird')
 var Video = mongoose.model('Video')
 var Audio = mongoose.model('Audio')
+var Creation = mongoose.model('Creation')
+var Promise = require('bluebird')
+var xss = require('xss')
 var robot = require('../service/robot')
 var config = require('../../config/config')
 
@@ -188,12 +190,77 @@ exports.save = function *(next) {
     var videoId = body.videoId
     var audioId = body.audioId
     var title = body.title
+    var user = this.session.user
 
-    console.log(videoId)
-    console.log(audioId)
-    console.log(title)
+    var video = yield Video.findOne({
+        _id: videoId
+    }).exec()
+    var audio = yield Audio.findOne({
+        _id: audioId
+    })
+
+    if (!video || !audio) {
+        this.body = {
+            success: false,
+            err: '音频或者视频素材不能为空'
+        }
+        return next
+    }
+
+    var creation = yield Creation.findOne({
+        audio: audioId,
+        video: videoId
+    }).exec()
+
+    if (!creation) {
+        var creationData = {
+            author: user._id,
+            title: xss(title),
+            audio: audioId,
+            video: videoId,
+            finish: 20
+        }
+
+        var video_public_id = video_public_id
+        var audio_public_id = audio_public_id
+
+        if (video_public_id && audio_public_id) {
+            creationData.cloudinary_thumb = 'http://res.cloudinary.com/db2oxpw9c/video/upload/' + video_public_id + '.jpg'
+            creationData.cloudinary_video = 'http://res.cloudinary.com/db2oxpw9c/video/upload/e_volume:-100/e_volume:400,l_video:' + audio_public_id.replace('/', ':') + '/' + video_public_id + '.mp4'
+            
+            creationData.finish += 20
+        }
+
+        if (audio.qiniu_thumb) {
+            creationData.qiniu_thumb = audio.qiniu_thumb
+            creationData.finish += 30
+        }
+
+        if (audio.qiniu_video) {
+            creationData.qiniu_video = audio.qiniu_video
+            creationData.finish += 30
+        }
+
+        creation = new Creation(creationData)
+    }
+
+    creation = yield creation.save()
 
     this.body = {
-        success: true
+        success: true,
+        data: {
+            _id: creation._id,
+            finish: creation.finish,
+            title: creation.title,
+            qiniu_thumb: creation.qiniu_thumb,
+            qiniu_video: creation.qiniu_video,
+            author: {
+                avatar: user.avatar,
+                nickname: user.nickname,
+                gender: user.gender,
+                breed: user.bread,
+                _id: user._id
+            }
+        }
     }
 }
