ae6b0040926559ab7498a2553bc151b8dc704345
Brave Chai <brave.chai@blackboard.com>
2017-11-13 14:52:11 +0800

    implement comment

--- config/routers.js
+++ config/routers.js
@@ -4,6 +4,7 @@ var Router = require('koa-router')
 var User = require('../app/controllers/user')
 var App = require('../app/controllers/app')
 var Creation = require('../app/controllers/creation')
+var Comment = require('../app/controllers/comment')
 
 module.exports = function() {
 	var router = new Router({
@@ -16,11 +17,17 @@ module.exports = function() {
 	router.post('/u/update', App.hasBody, App.hasToken, User.update)
 
 	// app
-	router.get('/creations', App.hasToken, Creation.find)
 	router.post('/signature', App.hasBody, App.hasToken, App.signature)
+
+	// creations
+	router.get('/creations', App.hasToken, Creation.find)
 	router.post('/creations/video', App.hasBody, App.hasToken, Creation.video)
 	router.post('/creations/audio', App.hasBody, App.hasToken, Creation.audio)
 	router.post('/creations', App.hasBody, App.hasToken, Creation.save)
+
+	// comments
+	router.get('/comments', App.hasToken, Comment.find)
+	router.post('/comments', App.hasBody, App.hasToken, Comment.save)
 
 	return router
 }
